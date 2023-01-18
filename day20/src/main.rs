36340bfa36d565d84c39db0e2264104dca139fcf
two-six <twopsix@gmail.com>
2023-01-18 19:13:17 +0100

    day21

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,3 +1,148 @@
+use regex::Regex;
+
+#[derive(Debug, Copy, Clone)]
+struct Particle {
+    position: (i64, i64, i64),
+    velocity: (i64, i64, i64),
+    acceleration: (i64, i64, i64),
+}
+
+impl Particle {
+    fn new(
+        position: (i64, i64, i64),
+        velocity: (i64, i64, i64),
+        acceleration: (i64, i64, i64),
+    ) -> Particle {
+        Particle {
+            position,
+            velocity,
+            acceleration,
+        }
+    }
+}
+
+impl PartialEq for Particle {
+    fn eq(&self, other: &Self) -> bool {
+        self.position.0 == other.position.0
+            && self.position.1 == other.position.1
+            && self.position.2 == other.position.2
+    }
+}
+
+impl Eq for Particle {}
+
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let particles = parse(&input);
+    println!("Silver: {}", silver(&particles));
+    println!("Gold: {}", gold(&particles));
+}
+
+fn gold(particles: &Vec<Particle>) -> usize {
+    let mut largest_n = 0;
+    for i in 0..particles.len() {
+        for j in i + 1..particles.len() {
+            if let Some(n) = find_n(&particles[i], &particles[j]) {
+                if n > largest_n {
+                    largest_n = n;
+                }
+            }
+        }
+    }
+    simulate(&particles, largest_n as usize).len()
+}
+
+fn silver(particles: &Vec<Particle>) -> usize {
+    particles
+        .iter()
+        .enumerate()
+        .min_by(|(_, x), (_, y)| {
+            (x.acceleration.0.abs() + x.acceleration.1.abs() + x.acceleration.2.abs())
+                .cmp(&(y.acceleration.0.abs() + y.acceleration.1.abs() + y.acceleration.2.abs()))
+        })
+        .map(|(i, _)| i)
+        .unwrap()
+}
+
+fn simulate(particles: &Vec<Particle>, n: usize) -> Vec<Particle> {
+    let mut result = remove_duplicates(&particles);
+    for i in 1..n + 1 {
+        result.iter_mut().for_each(|p| {
+            p.position.0 += p.velocity.0 + i as i64 * p.acceleration.0;
+            p.position.1 += p.velocity.1 + i as i64 * p.acceleration.1;
+            p.position.2 += p.velocity.2 + i as i64 * p.acceleration.2;
+        });
+        result = remove_duplicates(&result);
+    }
+    result
+}
+
+fn remove_duplicates(particles: &Vec<Particle>) -> Vec<Particle> {
+    let mut result: Vec<Particle> = Vec::new();
+    for i in 0..particles.len() {
+        let mut unique = true;
+        for j in 0..particles.len() {
+            if particles[i] == particles[j] && i != j {
+                unique = false;
+                break;
+            }
+        }
+        if unique {
+            result.push(particles[i]);
+        }
+    }
+    result
+}
+
+fn find_n(p1: &Particle, p2: &Particle) -> Option<i64> {
+    let mut prev_diff = i64::max_value();
+    let mut x0 = p1.position.0;
+    let mut y0 = p1.position.1;
+    let mut z0 = p1.position.2;
+    let mut x1 = p2.position.0;
+    let mut y1 = p2.position.1;
+    let mut z1 = p2.position.2;
+    for n in 1.. {
+        if x0 == x1 && y0 == y1 && z0 == z1 {
+            return Some(n);
+        }
+        x0 += p1.velocity.0 + n * p1.acceleration.0;
+        y0 += p1.velocity.1 + n * p1.acceleration.1;
+        z0 += p1.velocity.2 + n * p1.acceleration.2;
+        x1 += p2.velocity.0 + n * p2.acceleration.0;
+        y1 += p2.velocity.1 + n * p2.acceleration.1;
+        z1 += p2.velocity.2 + n * p2.acceleration.2;
+        let diff = (x0 - x1).abs() + (y0 - y1).abs() + (z0 - z1).abs();
+        if diff >= prev_diff {
+            break;
+        }
+        prev_diff = diff;
+    }
+    None
+}
+
+fn parse(input: &str) -> Vec<Particle> {
+    let regex =
+        Regex::new(r"p=<(.*?),(.*?),(.*?)>,\s+v=<(.*?),(.*?),(.*?)>,\s+a=<(.*?),(.*?),(.*?)>")
+            .unwrap();
+    let mut result: Vec<Particle> = Vec::new();
+    for cap in regex.captures_iter(input) {
+        let position = (
+            cap[1].parse::<i64>().unwrap(),
+            cap[2].parse::<i64>().unwrap(),
+            cap[3].parse::<i64>().unwrap(),
+        );
+        let velocity = (
+            cap[4].parse::<i64>().unwrap(),
+            cap[5].parse::<i64>().unwrap(),
+            cap[6].parse::<i64>().unwrap(),
+        );
+        let acceleration = (
+            cap[7].parse::<i64>().unwrap(),
+            cap[8].parse::<i64>().unwrap(),
+            cap[9].parse::<i64>().unwrap(),
+        );
+        result.push(Particle::new(position, velocity, acceleration));
+    }
+    result
 }
--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,3 +1,228 @@
+use std::collections::HashMap;
+
+const INITIAL_SQUARE: &str = ".#./..#/###";
+
+#[derive(Debug, Clone, PartialEq)]
+struct Square {
+    square: Vec<Vec<bool>>,
+}
+
+impl Square {
+    fn new() -> Square {
+        Square { square: Vec::new() }
+    }
+
+    fn count_turned_on(&self) -> usize {
+        self.square
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .fold(0, |acc, next| if *next { acc + 1 } else { acc })
+            })
+            .fold(0, |acc, row| acc + row)
+    }
+
+    fn rotate(&mut self) {
+        let mut tmp_square = vec![Vec::new(); self.square.len()];
+        for i in 1..=self.square.len() {
+            for j in 0..self.square.len() {
+                tmp_square[j].push(self.square[self.square.len() - i][j]);
+            }
+        }
+        self.square = tmp_square;
+    }
+
+    fn flip(&mut self) {
+        let mut tmp_square = vec![vec![false; self.square.len()]; self.square.len()];
+        for i in 0..self.square.len() {
+            for j in 0..self.square.len() {
+                tmp_square[i][j] = self.square[i][self.square.len() - 1 - j];
+            }
+        }
+        self.square = tmp_square;
+    }
+
+    fn from(pattern: &str) -> Square {
+        let n = pattern
+            .chars()
+            .fold(0, |acc, c| if c == '/' { acc + 1 } else { acc })
+            + 1;
+        let mut result = vec![Vec::new(); n];
+        pattern
+            .split('/')
+            .map(|line| line.chars())
+            .enumerate()
+            .for_each(|(i, line)| {
+                line.for_each(|c| {
+                    result[i].push(if c == '#' { true } else { false });
+                });
+            });
+        Square { square: result }
+    }
+
+    fn divide(&self) -> Board {
+        let mut result = Board::new();
+        if let Some(s) = self.square.get(0) {
+            let n = s.len();
+            if n % 2 == 0 {
+                let n = n / 2;
+                result.squares = vec![
+                    vec![
+                        Square {
+                            square: vec![vec![false, false]; 2]
+                        };
+                        n
+                    ];
+                    n
+                ];
+                for i in 0..n {
+                    for j in 0..n {
+                        for y in 0..2 {
+                            for x in 0..2 {
+                                // println!("2 -> i: {}, j: {}, y: {}, x: {}", i, j, y, x);
+                                result.squares[i][j].square[y][x] =
+                                    self.square[i * 2 + y][j * 2 + x];
+                            }
+                        }
+                    }
+                }
+            } else {
+                let n = n / 3;
+                result.squares = vec![
+                    vec![
+                        Square {
+                            square: vec![vec![false, false, false]; 3]
+                        };
+                        n
+                    ];
+                    n
+                ];
+                for i in 0..n {
+                    for j in 0..n {
+                        for y in 0..3 {
+                            for x in 0..3 {
+                                // println!("3 -> i: {}, j: {}, y: {}, x: {}", i, j, y, x);
+                                result.squares[i][j].square[y][x] =
+                                    self.square[i * 3 + y][j * 3 + x];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        result
+    }
+
+    fn turn_to_pattern(&self) -> String {
+        let mut result: String = String::from("");
+        self.square.iter().for_each(|row| {
+            row.iter().for_each(|c| {
+                if *c {
+                    result += "#";
+                } else {
+                    result += ".";
+                }
+            });
+            result += "/";
+        });
+        result.remove(result.len() - 1);
+        result
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Board {
+    squares: Vec<Vec<Square>>,
+}
+
+impl Board {
+    fn new() -> Board {
+        Board {
+            squares: Vec::new(),
+        }
+    }
+
+    fn flatten(&self) -> Square {
+        let n = self.squares.len();
+        let square_len = self.squares.get(0).unwrap().get(0).unwrap().square.len();
+        let mut result: Square = Square {
+            square: vec![vec![false; square_len * n]; square_len * n],
+        };
+        self.squares.iter().enumerate().for_each(|(i, column)| {
+            column.iter().enumerate().for_each(|(j, row)| {
+                row.square.iter().enumerate().for_each(|(y, square_row)| {
+                    square_row.iter().enumerate().for_each(|(x, square)| {
+                        result.square[i * square_len + y][j * square_len + x] = *square;
+                    });
+                });
+            });
+        });
+        result
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let patterns = create_rulebook(&input);
+    let result = solve(&patterns);
+    println!("Silver: {}", result.0);
+    println!("Gold: {}", result.1);
+}
+
+fn create_rulebook(input: &str) -> HashMap<String, String> {
+    let mut patterns: HashMap<String, String> = input
+        .lines()
+        .map(|line| {
+            let result = line.split_once(" => ").unwrap();
+            (result.0.to_owned(), result.1.to_owned())
+        })
+        .collect();
+    let keys: Vec<String> = patterns.keys().map(|k| k.clone()).collect();
+    for key in keys {
+        let pattern = patterns.get(&key).unwrap().clone();
+        let mut square_tmp = Square::from(&key);
+        square_tmp.flip();
+        patterns.insert(square_tmp.turn_to_pattern(), pattern.to_owned());
+        square_tmp.flip();
+        for _ in 0..3 {
+            square_tmp.rotate();
+            patterns.insert(square_tmp.turn_to_pattern(), pattern.to_owned());
+            square_tmp.flip();
+            patterns.insert(square_tmp.turn_to_pattern(), pattern.to_owned());
+            square_tmp.flip();
+        }
+    }
+    patterns
+}
+
+fn solve(patterns: &HashMap<String, String>) -> (usize, usize) {
+    let mut result = Square::from(INITIAL_SQUARE);
+    let mut silver = 0;
+    for i in 0..18 {
+        let mut board = result.divide();
+        board.squares = board
+            .squares
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .map(|square| {
+                        let mut result = Square::new();
+                        let square_pattern = square.turn_to_pattern();
+                        if patterns.contains_key(&square_pattern) {
+                            result = Square::from(patterns.get(&square_pattern).unwrap());
+                        }
+                        if result == Square::new() {
+                            panic!("couldn't find any pattern");
+                        }
+                        return result;
+                    })
+                    .collect()
+            })
+            .collect();
+        result = board.flatten();
+        if i == 4 {
+            silver = result.count_turned_on();
+        }
+    }
+    (silver, result.count_turned_on())
 }
